d954fba0ff4ebb80d031f5fb20358c7624c6c119
geraldinenasa <67355396+geraldinenasa@users.noreply.github.com>
2023-05-09 19:55:15 -0300

    Create higieneDeManos.java

---
+++ higieneDeManos.java
@@ -0,0 +1,21 @@
+package higieneDeManos;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        // Declaro tipo de variables antes de llamar inicializar variable
+        String paso1, paso2, paso3, paso4, paso5;
+        System.out.println("5 pasos para la higiene de manos");
+        paso1="Mojarse las manos con agua de la red";
+        System.out.println(paso1);
+        paso2="Enjabonar las manos incluyendo las muñecas";
+        System.out.println(paso2);
+        paso3="Frotar durante 40 a 60 segundos";
+        System.out.println(paso3);
+        paso4="Enjuagar con abudante agua";
+        System.out.println(paso4);
+        paso5="Secar con una toalla limpia";
+        System.out.println(paso5);
+    }
+    
+}
